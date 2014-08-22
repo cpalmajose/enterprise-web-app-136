79a03daed22dcb1c50085cc5ba377f8f37233c04
abfarin <abfarin@ucsd.edu>
2014-08-21 19:28:30 -0700

    New service file
    Added the TaTutorService.cs file to BL directory

---
+++ SL136/BL/TaTutorService.cs
@@ -0,0 +1,69 @@
+namespace Service
+{
+    using System;
+    using System.Collections.Generic;
+    using IRepository;
+    using POCO;
+
+    public class TaTutorService
+    {
+        private readonly ITaTutorRepository repository;
+
+        public TaTutorService(ITaTutorRepository repository)
+        {
+            this.repository = repository;
+        }
+		
+		public void InsertTaTutor(TaTutor ta_tutor, ref List<string> errors)
+		{
+            if (ta_tutor == null)
+            {
+                errors.Add("TA/Tutor cannot be null");
+                throw new ArgumentException();
+            }
+
+            if (ta_tutor.TaTutorId.Length < 5)
+            {
+                errors.Add("Invalid TA/Tutor ID");
+                throw new ArgumentException();
+            }
+
+            this.repository.InsertTaTutor(ta_tutor, ref errors);
+		}
+		
+        public void DeleteTaTutor(string ta_tutor_id, ref List<string> errors)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                errors.Add("Invalid TA/Tutor id");
+                throw new ArgumentException();
+            }
+
+            this.repository.DeleteTaTutor(ta_tutor_id, ref errors);
+        }
+		
+        public void UpdateTaTutor(TaTutor ta_tutor, ref List<string> errors)
+        {
+            if (ta_tutor == null)
+            {
+                errors.Add("TA/Tutor cannot be null");
+                throw new ArgumentException();
+            }
+
+            if (string.IsNullOrEmpty(ta_tutor.TaTutorId))
+            {
+                errors.Add("Invalid TA/Tutor id");
+                throw new ArgumentException();
+            }
+
+            if (ta_tutor.TaTutorId.Length < 5)
+            {
+                errors.Add("Invalid TA/Tutor id");
+                throw new ArgumentException();
+            }
+
+            this.repository.UpdateTaTutor(ta_tutor, ref errors);
+        }
+		
+	}
+}
